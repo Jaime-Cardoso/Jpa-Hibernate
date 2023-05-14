d731022a88da217d3f29e2c4b0698efd9edde971
Jaime Cardoso <70758067+Jaime-Cardoso@users.noreply.github.com>
2023-05-14 01:06:47 -0300

    H2 database, test profile, JPA

--- src/main/java/br/com/cursonelio/nelio/entities/User.java
+++ src/main/java/br/com/cursonelio/nelio/entities/User.java
@@ -1,10 +1,20 @@
 package br.com.cursonelio.nelio.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
