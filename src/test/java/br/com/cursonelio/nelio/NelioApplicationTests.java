ee72e50c3167dee30a319c877b980195ab6bbbeb
Jaime Cardoso <70758067+Jaime-Cardoso@users.noreply.github.com>
2023-05-14 00:30:46 -0300

    Crated

---
+++ src/test/java/br/com/cursonelio/nelio/NelioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cursonelio.nelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
