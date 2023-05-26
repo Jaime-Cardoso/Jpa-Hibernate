947e0d79a56ef5c26fb22696db3e2a6d16f655af
Jaime Cardoso <70758067+Jaime-Cardoso@users.noreply.github.com>
2023-05-26 18:03:59 -0300

    Category

--- src/main/java/br/com/cursonelio/nelio/config/testConfig.java
+++ src/main/java/br/com/cursonelio/nelio/config/testConfig.java
@@ -1,8 +1,10 @@
 package br.com.cursonelio.nelio.config;
 
+import br.com.cursonelio.nelio.entities.Category;
 import br.com.cursonelio.nelio.entities.Order;
 import br.com.cursonelio.nelio.entities.User;
 import br.com.cursonelio.nelio.entities.enums.OrderStatus;
+import br.com.cursonelio.nelio.repository.CategoryRepository;
 import br.com.cursonelio.nelio.repository.OrderRepository;
 import br.com.cursonelio.nelio.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +24,19 @@ public class testConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
         userRepository.saveAll(Arrays.asList(u1, u2));
---
+++ src/main/java/br/com/cursonelio/nelio/service/CategoryService.java
@@ -0,0 +1,27 @@
+package br.com.cursonelio.nelio.service;
+
+import br.com.cursonelio.nelio.entities.Category;
+import br.com.cursonelio.nelio.entities.User;
+import br.com.cursonelio.nelio.repository.CategoryRepository;
+import br.com.cursonelio.nelio.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Category findById(Long id){
+        Optional<Category> obj = categoryRepository.findById(id);
+        return obj.get();
+    }
+}
