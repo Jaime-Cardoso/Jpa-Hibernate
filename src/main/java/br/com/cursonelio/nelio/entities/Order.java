cd37608cf6f9c8f2bbd95608cf8dff295c5f314a
Jaime Cardoso <70758067+Jaime-Cardoso@users.noreply.github.com>
2023-05-26 20:24:49 -0300

    Payment, one-to-one association

--- src/main/java/br/com/cursonelio/nelio/entities/Order.java
+++ src/main/java/br/com/cursonelio/nelio/entities/Order.java
@@ -2,6 +2,7 @@ package br.com.cursonelio.nelio.entities;
 
 import br.com.cursonelio.nelio.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 import java.io.Serializable;
@@ -34,6 +36,9 @@ public class Order implements Serializable {
 
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> item = new HashSet<>();
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order() {
     }
@@ -81,6 +86,14 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItem() {
         return item;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
