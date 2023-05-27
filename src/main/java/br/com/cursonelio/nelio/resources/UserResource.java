33bdb2d7e9a7cba36105a97af7cc48a841b6c9dd
Jaime Cardoso <70758067+Jaime-Cardoso@users.noreply.github.com>
2023-05-26 21:08:11 -0300

    User insert

--- src/main/java/br/com/cursonelio/nelio/resources/UserResource.java
+++ src/main/java/br/com/cursonelio/nelio/resources/UserResource.java
@@ -4,11 +4,16 @@ import br.com.cursonelio.nelio.entities.User;
 import br.com.cursonelio.nelio.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,5 +30,11 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id){
         User obj = userService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
